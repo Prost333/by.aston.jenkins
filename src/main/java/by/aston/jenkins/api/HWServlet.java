bec04849a52e3ace54982eb7830b7c7431d1f3d8
prost333 <dimabolshoy333@yandex.by>
2024-05-21 22:24:07 +0300

    Initial commit

---
+++ src/main/java/by/aston/jenkins/api/HWServlet.java
@@ -0,0 +1,21 @@
+package by.aston.jenkins.api;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HWServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello  World12");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello123 World");
+    }
+}
